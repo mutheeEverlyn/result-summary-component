3e0b5c8a94c229b28c204b752b7ef49d0ae39064
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-09-01 23:15:25 +0300

    changes

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-const reaction =document.querySelector('.reaction');
-const memory =document.querySelector('.memory');
-const verbal =document.querySelector('.verbal');
-const visual =document.querySelector('.visual');
-document.addEventListener((e)=>onload,{
-fetch('data.json')
-.then(response =>(response.json))
-.then(data =>(data))
-.catch(console.log('data not found'))
+
+const icon1 =document.querySelector('#icon1');
+const name1 =document.querySelector('#name1');
+const marks1 =document.querySelector('#marks1');
+
+const icon2 =document.querySelector('#icon2');
+const name2 =document.querySelector('#name2');
+const marks2 =document.querySelector('#marks2');
+
+const icon3 =document.querySelector('#icon3');
+const name3 =document.querySelector('#name3');
+const marks3 =document.querySelector('#marks3');
+
+const icon4 =document.querySelector('#icon4');
+const name4 =document.querySelector('#name4');
+const marks4 =document.querySelector('#marks4');
+// document.addEventListener("load",{
+// fetch("data.json").then(response =>(response.json))
+// .then(data =>(console.log(data)))
+// .catch(console.log('data not found'))
+// })
+fetch ("./data.json")
+.then(res=>res.json())
+.then(
+    data=>{
+        icon1.textContent=` ${ data[0].icon}`;
+         name1.textContent=` ${ data[0].category}`
+         marks1.textContent=` ${ data[0].score}/100`;
+
+       
+    }
+)
+.catch((error)=>{
+    console.log(error)
 })
